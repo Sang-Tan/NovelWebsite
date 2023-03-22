fc8f3e591011921cb1070d6ab0a75bb261bf5575
SPRV <sangprovai14@gmail.com>
2023-03-22 17:45:41 +0700

    Refactor : refactor BaseRepository
    Make base repository be singleton
    Add some conventional functions in BaseRepository

--- src/main/java/core/database/BaseRepository.java
+++ src/main/java/core/database/BaseRepository.java
@@ -1,48 +1,114 @@
 package core.database;
 
-import core.database.SqlRecord;
+import core.DatabaseObject;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.HashMap;
-import java.util.Set;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
-public abstract class BaseRepository {
-    private String TABLE_NAME;
+public abstract class BaseRepository<T extends DatabaseObject> {
+    private final String TABLE_NAME;
 
-    protected abstract void setTableName();
+    private final String[] PRIMARY_KEY;
 
-    private BaseRepository() {
-        setTableName();
+    protected BaseRepository(String tableName, String[] primaryKey) {
+        this.TABLE_NAME = tableName;
+        this.PRIMARY_KEY = primaryKey;
     }
 
+    protected String getTableName() {
+        return this.TABLE_NAME;
+    }
 
-    public void insert(SqlRecord record) throws SQLException {
-        Object[] columns = record.getColumns().toArray();
-        String columnsString = "";
-        String valuesString = "";
+    protected String[] getPrimaryKey() {
+        return this.PRIMARY_KEY;
+    }
 
-        //columnsString col1, col2, col3,...
-        //valuesString ?, ?, ?,...
-        for (int i = 0; i < columns.length; i++) {
-            columnsString += columns[i];
-            valuesString += "?";
-            if (i < columns.length - 1) {
-                columnsString += ", ";
-                valuesString += ", ";
-            }
+    /**
+     * Get the primary key string separated by comma, e.g. "id, name"
+     *
+     * @return
+     */
+    protected String getPrimaryKeyString() {
+        return String.join(", ", this.PRIMARY_KEY);
+    }
+
+    /**
+     * Map a row in the result set to a bean
+     *
+     * @param resultSet the result set
+     * @return the bean
+     * @throws SQLException if a database access error occurs
+     */
+    protected abstract T mapRow(ResultSet resultSet) throws SQLException;
+
+    protected abstract SqlRecord mapObject(T object);
+
+    protected List<T> mapRows(ResultSet resultSet) throws SQLException {
+        List<T> list = new ArrayList<>();
+        while (resultSet.next()) {
+            list.add(mapRow(resultSet));
         }
+        return list;
+    }
+
+    public List<T> getAll() throws SQLException {
+        String sql = String.format("SELECT * FROM %s", getTableName());
+        ResultSet resultSet = MySQLdb.getInstance().query(sql);
+        return mapRows(resultSet);
+    }
+
+    public List<T> getRange(int start, int end) throws SQLException {
+        String sql = String.format("SELECT * FROM %s LIMIT %d, %d", getTableName(), start, end);
+        ResultSet resultSet = MySQLdb.getInstance().query(sql);
+        return mapRows(resultSet);
+    }
+
+    
+    public void insert(T object) throws SQLException {
+        SqlRecord record = mapObject(object);
+        insert(record);
+    }
+
+    /**
+     * Insert a record into the database
+     *
+     * @param record the record to insert
+     * @throws SQLException if a database access error occurs
+     */
+    protected void insert(SqlRecord record) throws SQLException {
+        int size = record.size();
+
+        //example columnsString = "col1, col2, col3,..."
+        String[] columns = new String[size];
+        record.getColumns().toArray(columns);
+        String columnsString = String.join(", ", columns);
+
+        //example valuesString = "?, ?, ?,..."
+        String[] values = new String[size];
+        Arrays.fill(values, "?");
+        String valuesString = String.join(", ", values);
+
+
         String sql = String.format("INSERT INTO %s (%s) VALUES (%s)", getTableName(), columnsString, valuesString);
 
-        Connection conn = MySQLdb.getInstance().getConnectDB();
-
-        PreparedStatement preparedStatement = conn.prepareStatement(sql);
-        for (int i = 0; i < columns.length; i++) {
-            preparedStatement.setObject(i + 1, record.getValue(columns[i].toString()));
-        }
-        preparedStatement.executeUpdate();
-
+        Object[] valuesArray = record.getValues(columns);
+        MySQLdb.getInstance().execute(sql, valuesArray);
     }
+
+
+    public Integer count() throws SQLException {
+        String sql = String.format("SELECT COUNT(%s) FROM %s", getPrimaryKeyString(), getTableName());
+        ResultSet resultSet = MySQLdb.getInstance().query(sql);
+        if (resultSet.next()) {
+            return resultSet.getInt(1);
+        }
+        return 0;
+    }
+
+
 }
