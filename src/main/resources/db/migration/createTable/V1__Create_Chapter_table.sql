2aae78dcdeb6be29bd4975d3f78e8b609cc150ce
SPRV <sangprovai14@gmail.com>
2023-03-24 14:03:18 +0700

    refactor : add collumn Name into chapters table

--- src/main/resources/db/migration/createTable/V1__Create_Chapter_table.sql
+++ src/main/resources/db/migration/createTable/V1__Create_Chapter_table.sql
@@ -1,10 +1,11 @@
 CREATE TABLE chapters
 (
     id          INT PRIMARY KEY AUTO_INCREMENT,
-    order_index INT       NOT NULL,
-    volume_id   INT       NOT NULL,
-    content     TEXT               DEFAULT NULL,
-    modify_time TIMESTAMP NOT NULL,
-    is_pending  BOOLEAN   NOT NULL DEFAULT TRUE,
+    name        VARCHAR(255) NOT NULL,
+    order_index INT          NOT NULL,
+    volume_id   INT          NOT NULL,
+    content     TEXT                  DEFAULT NULL,
+    modify_time TIMESTAMP    NOT NULL,
+    is_pending  BOOLEAN      NOT NULL DEFAULT TRUE,
     UNIQUE (order_index, volume_id)
 );
