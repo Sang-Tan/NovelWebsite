bd304c451d7f72cc27509fd4bc17b6c3b4bedcde
Trần Sĩ  Tiến <106511449+TranSiTien@users.noreply.github.com>
2023-03-25 10:18:40 +0700

    Initial Logout

--- src/main/java/controller/authentication/Logout.java
+++ src/main/java/controller/authentication/Logout.java
@@ -1,17 +1,62 @@
 package controller.authentication;
 
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-import java.io.IOException;
+import core.JSON;
+import repository.TokenRepository;
+import service.validator.UserValidator;
+import repository.UserRepository;
+import model.User;
+import org.json.JSONException;
 
+import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.logging.ConsoleHandler;
+import java.util.logging.Handler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+@MultipartConfig
 @WebServlet(name = "LogoutServlet", value = "/logout")
 public class Logout extends HttpServlet {
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-        HttpSession session = request.getSession(true);
+    private static final Logger LOGGER = Logger.getLogger(Logout.class.getName());
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+        LOGGER.setLevel(Level.ALL);
+        Handler handler = new ConsoleHandler();
+        handler.setLevel(Level.ALL);
+        LOGGER.addHandler(handler);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws IOException {
+
+
+    // Remove the user's credentials from the session
+        HttpSession session = request.getSession();
+        session.removeAttribute("username");
+        session.removeAttribute("password");
+
+        //delete token from database
+        try {
+            TokenRepository.getInstance().deleteToken(request.getParameter("token"));
+        } catch (SQLException e) {
+            response.setStatus(500);
+            Logout.LOGGER.warning("token not found");
+        }
+
+        // Expire the token cookie
+        Cookie tokenCookie = new Cookie("token", "");
+        tokenCookie.setMaxAge(0);
+        response.addCookie(tokenCookie);
 
     }
+
+
 }
