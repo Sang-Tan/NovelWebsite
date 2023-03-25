a13620e893137aeb0ad26a7f9d14d34f90044a0a
SPRV <sangprovai14@gmail.com>
2023-03-25 15:19:48 +0700

    refactor : add expired time column

--- src/main/resources/db/migration/createTable/V1__Create_token_table.sql
+++ src/main/resources/db/migration/createTable/V1__Create_token_table.sql
@@ -1,8 +1,10 @@
 CREATE TABLE tokens
 (
-    id             INT PRIMARY KEY AUTO_INCREMENT,
-    user_id        INT      NOT NULL,
-    validator_hash CHAR(32) NOT NULL,
-    UNIQUE (validator_hash)
+    id           INT PRIMARY KEY AUTO_INCREMENT,
+    user_id      INT       NOT NULL,
+    token_hash   CHAR(64)  NOT NULL,
+    expired_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    UNIQUE (token_hash)
+
 );
 
