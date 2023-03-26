c2207e2496a54472eb6b90b29acc374429459116
Trần Sĩ  Tiến <106511449+TranSiTien@users.noreply.github.com>
2023-03-26 14:35:55 +0700

    chore : minor change to database

--- src/main/resources/db/migration/createTable/V1__Create_token_table.sql
+++ src/main/resources/db/migration/createTable/V1__Create_token_table.sql
@@ -1,10 +1,9 @@
 CREATE TABLE tokens
 (
-    id           INT PRIMARY KEY AUTO_INCREMENT,
-    user_id      INT       NOT NULL,
-    token_hash   CHAR(64)  NOT NULL,
-    expired_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    id         INT PRIMARY KEY AUTO_INCREMENT,
+    user_id    INT      NOT NULL,
+    token_hash CHAR(64) NOT NULL,
+    expired_time    DATETIME NOT NULL,
     UNIQUE (token_hash)
-
 );
 
