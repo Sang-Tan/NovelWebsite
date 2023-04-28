9f75cc505468c846fbbbfb6ea632ad4e44423d7b
SPRV <sangprovai14@gmail.com>
2023-04-28 08:03:00 +0700

    chores : change volumes table

--- src/main/resources/db/migration/createTable/V1__Create_Volume_table.sql
+++ src/main/resources/db/migration/createTable/V1__Create_Volume_table.sql
@@ -1,11 +1,12 @@
 CREATE TABLE volumes
 (
-    id          INT PRIMARY KEY AUTO_INCREMENT,
-    novel_id    INT          NOT NULL,
-    name        VARCHAR(255) NOT NULL,
-    image       VARCHAR(255),
-    order_index INT          NOT NULL,
+    id              INT PRIMARY KEY AUTO_INCREMENT,
+    novel_id        INT          NOT NULL,
+    name            VARCHAR(255) NOT NULL,
+    image           VARCHAR(255),
+    order_index     INT          NOT NULL,
     approval_status VARCHAR(20)  NOT NULL DEFAULT 'pending',
+    updated_at      TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     CHECK (approval_status IN ('pending', 'approved', 'rejected')),
-        UNIQUE (novel_id, order_index)
+    UNIQUE (novel_id, order_index)
 );
