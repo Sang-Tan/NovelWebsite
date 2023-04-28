832d0cf34e162fc62ffe624259a6e0d6b3e6d124
SPRV <sangprovai14@gmail.com>
2023-04-28 08:04:10 +0700

    chores : change chapters table

--- src/main/resources/db/migration/createTable/V1__Create_Chapter_table.sql
+++ src/main/resources/db/migration/createTable/V1__Create_Chapter_table.sql
@@ -5,7 +5,7 @@ CREATE TABLE chapters
     order_index     INT          NOT NULL,
     volume_id       INT          NOT NULL,
     content         TEXT                  DEFAULT NULL,
-    modify_time     TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
+    update_at       TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
     approval_status VARCHAR(20)  NOT NULL DEFAULT 'pending',
     CHECK (approval_status IN ('pending', 'approved', 'rejected')),
     UNIQUE (order_index, volume_id)
