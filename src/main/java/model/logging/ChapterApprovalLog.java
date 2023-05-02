fd8c68a5201d2a106a42579badc047acb04a01ae
SPRV <sangprovai14@gmail.com>
2023-05-02 15:20:46 +0700

    feat : create logs for Novel, Volume, Chapter

---
+++ src/main/java/model/logging/ChapterApprovalLog.java
@@ -0,0 +1,80 @@
+package model.logging;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+import java.util.Objects;
+
+@Entity
+@Table(name = "chapter_approval_logs")
+public class ChapterApprovalLog implements IApprovalLog {
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @Column(name = "id")
+    private Integer id;
+    @Basic
+    @Column(name = "chapter_id")
+    private Integer chapterId;
+    @Basic
+    @Column(name = "moderator_id")
+    private Integer moderatorId;
+    @Basic
+    @Column(name = "content")
+    private String content;
+    @Basic
+    @Column(name = "created_at")
+    private Timestamp createdTime;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getChapterId() {
+        return chapterId;
+    }
+
+    public void setChapterId(Integer chapterId) {
+        this.chapterId = chapterId;
+    }
+
+    public Integer getModeratorId() {
+        return moderatorId;
+    }
+
+    public void setModeratorId(Integer moderatorId) {
+        this.moderatorId = moderatorId;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Timestamp getCreatedTime() {
+        return createdTime;
+    }
+
+    public void setCreatedTime(Timestamp createdAt) {
+        this.createdTime = createdAt;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ChapterApprovalLog that = (ChapterApprovalLog) o;
+        return Objects.equals(id, that.id) && Objects.equals(chapterId, that.chapterId) && Objects.equals(moderatorId, that.moderatorId) && Objects.equals(content, that.content) && Objects.equals(createdTime, that.createdTime);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, chapterId, moderatorId, content, createdTime);
+    }
+}
---
+++ src/main/java/model/logging/VolumeApprovalLog.java
@@ -0,0 +1,79 @@
+package model.logging;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+import java.util.Objects;
+
+@Entity
+@Table(name = "volume_approval_logs")
+public class VolumeApprovalLog implements IApprovalLog {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @Column(name = "id")
+    private Integer id;
+    @Basic
+    @Column(name = "volume_id")
+    private Integer volumeId;
+    @Basic
+    @Column(name = "moderator_id")
+    private Integer moderatorId;
+    @Basic
+    @Column(name = "content")
+    private String content;
+    @Basic
+    @Column(name = "created_at")
+    private Timestamp createdTime;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getVolumeId() {
+        return volumeId;
+    }
+
+    public void setVolumeId(Integer volumeId) {
+        this.volumeId = volumeId;
+    }
+
+    public Integer getModeratorId() {
+        return moderatorId;
+    }
+
+    public void setModeratorId(Integer moderatorId) {
+        this.moderatorId = moderatorId;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Timestamp getCreatedTime() {
+        return createdTime;
+    }
+
+    public void setCreatedTime(Timestamp createdAt) {
+        this.createdTime = createdAt;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        VolumeApprovalLog that = (VolumeApprovalLog) o;
+        return Objects.equals(id, that.id) && Objects.equals(volumeId, that.volumeId) && Objects.equals(moderatorId, that.moderatorId) && Objects.equals(content, that.content) && Objects.equals(createdTime, that.createdTime);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, volumeId, moderatorId, content, createdTime);
+    }
+}
