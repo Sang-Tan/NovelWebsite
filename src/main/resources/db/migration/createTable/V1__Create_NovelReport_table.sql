bce1d6f29aecbed0ea128c46baea0fc02ad3cbaf
minhcuong2102 <cuong21022003@gmail.com>
2023-05-02 14:34:12 +0700

    update novel_report.jsp

--- src/main/resources/db/migration/createTable/V1__Create_NovelReport_table.sql
+++ src/main/resources/db/migration/createTable/V1__Create_NovelReport_table.sql
@@ -5,5 +5,6 @@ CREATE TABLE novel_report
     reporter_id INT          NOT NULL,
     reason      VARCHAR(255) NOT NULL,
     check_time  TIMESTAMP    NULL DEFAULT NULL,
+    report_time TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
     UNIQUE (novel_id, reporter_id)
 );
