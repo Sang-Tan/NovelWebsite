3d4c46456ab0f5eb44c91599a6c7189026c0f114
SPRV <sangprovai14@gmail.com>
2023-06-05 16:10:57 +0700

    style: better landing page

--- src/main/java/controller/HomeController.java
+++ src/main/java/controller/HomeController.java
@@ -1,6 +1,8 @@
 package controller;
 
+import model.Chapter;
 import model.Novel;
+import repository.ChapterRepository;
 import service.ViewService;
 import service.validator.NovelSearchService;
 
@@ -11,7 +13,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @WebServlet(name = "HomeController", value = "")
 public class HomeController extends HttpServlet {
@@ -21,20 +25,27 @@ public class HomeController extends HttpServlet {
         List<Novel> topViewRecentWeekNovels = null;
         List<Novel> topViewRecentMonthNovels = null;
         List<Novel> topViewRecentDayNovels = null;
+        Map<Integer, Chapter> latestUpdateChapters = new HashMap<>();
 
         try {
-             latestUpdateNovels = NovelSearchService.getInstance().getLatestUpdateNovels(12);
-             topViewRecentWeekNovels = ViewService.getInstance().getTopViewRecentWeekNovels(4);
-             topViewRecentMonthNovels = ViewService.getInstance().getTopViewRecentMonthNovels(4);
-             topViewRecentDayNovels = ViewService.getInstance().getTopViewRecentDayNovels(4);
-        }
-        catch (Exception e) {
+            latestUpdateNovels = NovelSearchService.getInstance().getLatestUpdateNovels(12);
+            topViewRecentWeekNovels = ViewService.getInstance().getTopViewRecentWeekNovels(4);
+            topViewRecentMonthNovels = ViewService.getInstance().getTopViewRecentMonthNovels(4);
+            topViewRecentDayNovels = ViewService.getInstance().getTopViewRecentDayNovels(4);
+
+            for (Novel novel : latestUpdateNovels) {
+                int novelId = novel.getId();
+                Chapter latestChapter = ChapterRepository.getInstance().getLastChapterOfNovel(novelId);
+                latestUpdateChapters.put(novelId, latestChapter);
+            }
+        } catch (Exception e) {
             e.printStackTrace();
         }
         request.setAttribute("latestUpdateNovels", latestUpdateNovels);
         request.setAttribute("topViewRecentWeekNovels", topViewRecentWeekNovels);
         request.setAttribute("topViewRecentMonthNovels", topViewRecentMonthNovels);
         request.setAttribute("topViewRecentDayNovels", topViewRecentDayNovels);
+        request.setAttribute("latestUpdateChapters", latestUpdateChapters);
 
         showHomePage(request, response);
     }
